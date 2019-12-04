8de1cc7b33177f660d90bbaff240e3326281666e
SofiaFredriksson <sofiapkf@gmail.com>
2019-12-04 20:14:15 +0000

    api key hidden for kittens, added img tag in index.html for kittenz

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const PORT = process.env.PORT || 3000;
+const express = require("express")
+const app = express()
+const http = require("http").createServer(app)
+const PORT = process.env.PORT || 3000
+require("dotenv").config()
 
-app.use(express.static("src"));
+app.use(express.static("src"))
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/src/index.html");
-});
+  res.sendFile(__dirname + "/src/index.html")
+})
 
-app.listen(PORT, console.log("Listening on port ", PORT));
+app.listen(PORT, console.log("Listening on port ", PORT))
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-const quoteEl = document.getElementById("quote");
+const quoteEl = document.getElementById("quote")
 
 const fetchQuotes = () => {
   return fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
     .then(res => res.json())
-    .then(quotes => renderQuote(quotes[0]));
-};
+    .then(quotes => renderQuote(quotes[0]))
+}
+
+// const fetchKittens = () => {
+//   return fetch("")
+// }
 
 const renderQuote = quote => {
-  quoteEl.innerText = quote;
-};
+  quoteEl.innerText = quote
+}
 
-fetchQuotes();
+fetchQuotes()
