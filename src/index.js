8dd2c1f96c7a22e62486f62e36209ce8f8e17395
Stuart Sewell <hello@stuartsewell.dev>
2019-12-04 21:00:23 +0000

    center button

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-const quoteEl = document.getElementById("quote")
-const imgEl = document.getElementById("kitten-img")
-const btnEl = document.getElementById("next-btn")
+const quoteEl = document.getElementById("quote");
+const imgEl = document.getElementById("kitten-img");
+const btnEl = document.getElementById("next-btn");
 
 const fetchQuotes = () => {
   return fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
     .then(res => res.json())
-    .then(quotes => renderQuote(quotes[0]))
-}
+    .then(quotes => renderQuote(quotes[0]));
+};
 
 const fetchKittens = () => {
   fetch("https://api.thecatapi.com/v1/images/search")
     .then(resp => resp.json())
-    .then(data => (imgEl.src = data[0].url))
-}
+    .then(data => (imgEl.src = data[0].url));
+};
 
 btnEl.addEventListener("click", () => {
-  fetchQuotes()
-  fetchKittens()
-})
+  fetchKittens();
+  fetchQuotes();
+});
 
 const renderQuote = quote => {
-  quoteEl.innerText = quote
-}
+  quoteEl.innerText = quote;
+};
 
-fetchQuotes()
-fetchKittens()
+fetchQuotes();
+fetchKittens();
